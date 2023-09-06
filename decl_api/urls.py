ae1e09af60a8fc7a5ff1a7e00d4e08b101b7dae9
Ilmag <50011058+Ilmag@users.noreply.github.com>
2023-09-06 20:00:10 +0400

    Debug 500 1

--- decl_api/urls.py
+++ decl_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from decl_api import views
 
 urlpatterns = [
-    path('/api', views.DeclarationList.as_view(),name='all'),
+    path('api/', views.DeclarationList.as_view(),name='all'),
 ]
